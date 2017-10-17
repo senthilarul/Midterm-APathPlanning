83bd65d86017939b6ecedaa187df850fb86bd9a4
senthilarul <senthilhariharana@gmail.com>
2017-10-17 00:01:21 -0700

    Task 2.1 added class description for Map class

---
+++ include/Map.hpp
@@ -0,0 +1,31 @@
+//map
+#include <iostream>
+#include <fstream>
+#include <sstream> //change
+#include <vector>
+#include <string.h>
+
+class Map {
+ private:
+    std::vector<int> currentMap;
+    int totalMap;
+    int column = 7;
+    int row = 4;
+    int moveDirection[8] = {-1,0,    // top
+                             0,1,    // right
+                             1,0,    // bottom
+                             0,-1};  // left 
+
+ public:
+    void displayMap();
+    void storeMap(std::vector<int> map);
+    void loadMap();
+    std::vector<int> getMap();
+    void deleteMap();
+    int returnColumn();  //auto
+    int returnRow();  //auto
+    void setColumn(int colCount);
+    void setRow(int rowCount);
+    std::vector<int> returnMap();
+    int* returnDirection();
+};
