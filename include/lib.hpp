ed4e9d0acf919b9a72c826e949c27e1c8c55124d
senthilarul <senthilhariharana@gmail.com>
2017-10-17 00:06:50 -0700

    Task 2.1 added coverage for all class .hpp files, their methods in .cpp files

--- include/lib.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include<iostream>
-#include <opencv2/opencv.hpp>
-void dummy()
-{
-    std::cout << "HI" << std::endl;
-}
--- test/test.cpp
+++
@@ -1,6 +0,0 @@
-#include <gtest/gtest.h>
-#include <gmock/gmock.h>
-
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
-}
