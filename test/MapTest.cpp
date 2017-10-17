3ec12b75769777bfc5ae1db37498225806cee74c
senthilarul <senthilhariharana@gmail.com>
2017-10-17 08:04:28 -0700

    removed dummy test and added test cases for Map class methods

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -1,6 +1,18 @@
 #include <gtest/gtest.h>
+#include <memory>
+#include "Map.hpp"
 
-TEST(dummy, should_pass)
+std::shared_ptr<Map> testMap = std::make_shared<Map>();
+std::vector<int> testMapArray1 {1,1,1,1,0,0,0,0,
+			        0,0,0,1,1,0,0,0,
+			        0,0,1,1,1,1,1,0,
+			        1,1,1,1,1,1,1,0};
+
+TEST(MapLoadMapTest, testLoadMapMethodWithDefaultMap)
 {
-    EXPECT_EQ(1, 1);
+    testMap->loadMap("../Data/data1.csv");
+    ASSERT_EQ(testMap->returnColumn(), 8);
+    ASSERT_EQ(testMap->returnRow(), 4);
+    ASSERT_EQ(testMap->getMap(), testMapArray1);
 }
+
