742b6fd4c7b0c407fc4566ed9d5550706d329f7a
senthilarul <senthilhariharana@gmail.com>
2017-10-17 05:33:13 -0700

    Task 2.2 implemented methods for class Map

---
+++ app/Map.cpp
@@ -0,0 +1,72 @@
+#include "Map.hpp"
+
+
+void Map::displayMap() {
+    std::vector<int> displayLayout = currentMap;
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < column; j++) {
+            if(displayLayout[i*column + j] == 1) {
+                std::cout << "o";
+            }
+            else {
+                std::cout << "x";
+            }
+        }
+    std::cout << std::endl;
+    }
+
+}
+
+void Map::storeMap(std::vector<int> map) {
+    currentMap = map;
+}
+
+void Map::loadMap() {
+    std::ifstream file("/home/viki/triala*/Data/data1.csv");
+    std::string row, cell;
+    int rowCount = 0;
+    if(file.good()) {
+        while(std::getline(file,row)) {
+            int columnCount = 0;
+            std::istringstream linestream(row);
+            while (getline(linestream, cell, ',')) {
+                ++columnCount;
+                if(cell == "0")
+                    currentMap.emplace_back(0);
+                else
+                    currentMap.emplace_back(1);
+            }
+            ++rowCount;
+            setColumn(columnCount);
+        }
+    }
+    setRow(rowCount);
+}
+
+std::vector<int> Map::getMap() {
+    return currentMap;
+}
+
+int Map::returnColumn() {
+    return column;
+}
+
+int Map::returnRow() {
+    return row;
+}
+
+void Map::setColumn(int colCount) {
+    column = colCount;
+}
+
+void Map::setRow(int rowCount) {
+    rowCount = row;
+}
+
+std::vector<int> Map::returnMap() {
+    return currentMap;
+}
+
+int* Map::returnDirection() {
+    return moveDirection;
+}
