ba35b083705a1b15c3f1aeacc5dbfa03cd94f586
senthilarul <senthilhariharana@gmail.com>
2017-10-17 05:40:17 -0700

    Task 2.3 implemented demo of astar pathplanning algorithm

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,47 @@
 #include <iostream>
-#include <lib.hpp>
+#include <memory>
+#include <list>
+#include <vector>
+#include "Astar.hpp"
 
-int main()
-{
-    dummy();
+#define debug
+
+int main() {
+    Astar a;
+    Map warehouseMap;
+    #ifdef debug
+    std::vector<int> mapArray {1,1,1,1,0,1,0,
+			       0,0,0,1,1,0,0,
+			       0,0,1,1,1,1,1,
+			       1,1,1,1,1,1,0};
+    #endif
+    int startPt,endPt;
+    int userChoice;
+    int noOfSteps;
+    // menu
+    std::cout<<"Astar Path Planning using C++"<<std::endl;
+    std::cout<<"Choose which map to use: "<<std::endl;
+    std::cout<<"1) default map"<<std::endl;
+    std::cout<<"2) load map"<<std::endl;
+    std::cin>>userChoice;
+    if(userChoice == 1) {
+        warehouseMap.storeMap(mapArray);
+        warehouseMap.displayMap();
+    }
+    else {
+        warehouseMap.loadMap();
+        warehouseMap.displayMap();
+    }
+    std::cout<<"Enter Start and End points: "<<std::endl;
+    std::cin>>startPt>>endPt;
+    bool success = a.createNodeList(warehouseMap, startPt-1,endPt-1);
+    if(success == true) {
+        noOfSteps = a.planPath();
+        
+    }
+    else {
+        std::cout<<"Enter a valid start and end point"<<std::endl;
+    }
+    std::cout<<"Number of Steps taken in the shortest path is: "<<noOfSteps;
     return 0;
 }
